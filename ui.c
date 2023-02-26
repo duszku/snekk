310ba06c04729dd8bf846d3b92cbb9fcbfc649d2
duszku <86882994+duszku@users.noreply.github.com>
2023-02-26 02:38:43 +0100

    Tidied up `ui.c`
    added some comments, moved the macro to the top of the file and created
    a separate function that aggregates all curses boilerplate code

--- ui.c
+++ ui.c
@@ -1,7 +1,8 @@
 #include "ui.h"
 
-static void         draw_empty(struct game *);
-static void         draw_map(struct game *);
+static void         curses_setup(void);         /* abstracts boilerplate code */
+static void         draw_empty(struct game *);  /* draws empty map w/ borders */
+static void         draw_map(struct game *);    /* draws snake & apple */
 
 void *
 ui_thread_r(void *v_game)
@@ -10,14 +11,10 @@ ui_thread_r(void *v_game)
 
         game = (struct game *)v_game;
 
-        initscr();
-        cbreak();
-        noecho();
-        clear();
-        refresh();
-        draw_empty(game);
+        curses_setup();
 
         while (!game->gameover) {
+                draw_empty(game);
                 draw_map(game);
                 sleep(1);
         }
@@ -28,26 +25,36 @@ ui_thread_r(void *v_game)
 }
 
 void
+curses_setup(void)
+{
+        initscr();
+        cbreak();
+        noecho();
+        clear();
+        refresh();
+}
+
+void
 draw_empty(struct game *g)
 {
-#define INSERT_CHAR(X, Y, C) do {   \
-        move((Y), (X));             \
-        delch();                    \
-        insch((C));                 \
-} while (0)
-
         unsigned x, y;
 
+        clear();
+        refresh();
+
+        /* draw vertical lines */
         for (y = 0; y < g->g_heig; ++y) {
                 INSERT_CHAR(0, y, '|');
                 INSERT_CHAR(g->g_widt - 1, y, '|');
         }
 
+        /* draw horizontal lines */
         for (x = 0; x < g->g_widt; ++x) {
                 INSERT_CHAR(x, 0, '-');
                 INSERT_CHAR(x, g->g_heig - 1, '-');
         }
 
+        /* draw corners */
         INSERT_CHAR(0, 0, '+');
         INSERT_CHAR(0, g->g_heig - 1, '+');
         INSERT_CHAR(g->g_widt - 1, 0, '+');

a7ca3f6e791633f3e469866f70390eefa0c43817
duszku <86882994+duszku@users.noreply.github.com>
2023-02-27 20:51:16 +0100

    Apple spawning added

--- logic.c
+++ logic.c
@@ -29,3 +29,26 @@ logic_entry_point(void *v_game)
 
         return NULL;
 }
+
+void
+spawn_apple(struct game *game, unsigned *seed_p)
+{
+        int      ap_x, ap_y;
+
+        do {
+                do ap_x = rand_r(seed_p);
+                while (ap_x < 1 || (unsigned)ap_x > game->g_widt - 1);
+
+                do ap_y = rand_r(seed_p);
+                while (ap_y < 1 || (unsigned)ap_y > game->g_heig - 1);
+        } while (0); /* TODO: check for snake collision */
+
+        if (pthread_mutex_lock(&(game->mt_apple)) != 0)
+                ERROR("pthread_mutex_lock");
+
+        *((int *)ftuple_fst(game->apple)) = ap_x;
+        *((int *)ftuple_snd(game->apple)) = ap_y;
+
+        if (pthread_mutex_unlock(&(game->mt_apple)) != 0)
+                ERROR("pthread_mutex_lock");
+}

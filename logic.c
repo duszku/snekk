15ef3ba8a4ccbb95c6be02a909268400212c8bac
duszku <86882994+duszku@users.noreply.github.com>
2023-02-27 20:02:17 +0100

    Started working on logic module
    all files in place, core function and includes ready, lets rock

---
+++ logic.c
@@ -0,0 +1,7 @@
+#include "logic.h"
+
+void *
+logic_entry_point(void *v_game)
+{
+        return NULL;
+}
---
+++ logic.h
@@ -0,0 +1,8 @@
+#ifndef LOGIC_H
+#define LOGIC_H
+
+#include "game.h"
+
+void        *logic_entry_point(void *);
+
+#endif /* LOGIC_H */

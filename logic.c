65ddb689635af4a3a12b12c2c3c15b19d3f761d5
duszku <86882994+duszku@users.noreply.github.com>
2023-03-01 19:42:11 +0100

    Fixed comment indent

--- logic.c
+++ logic.c
@@ -6,7 +6,7 @@
  * previous snake segment, used for movement in pull_snake(). it is stored as
  * global state because flist_map() exposes list for mapping only with
  * single-argument functions.
- * */
+ */
 struct ftuple   *movement_prev;
 
 static void      move_snake(struct game *);

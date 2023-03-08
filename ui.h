dd55180551b682a7e0f9d809a907ab96d59c40ff
duszku <86882994+duszku@users.noreply.github.com>
2023-03-08 19:40:06 +0100

    Code commenting

--- ui.h
+++ ui.h
@@ -8,9 +8,17 @@
 #define UI_H
 
 #include "game.h"
-
 #include <curses.h>
 
+/*
+ * This file serves one purpose and one purpose only and it is to expose this
+ * particular subroutine to the main thread. Actual implementation is hidden
+ * within `ui.c` to increase modularity of the code and make any future
+ * refactorings simpler.
+ *
+ * As name suggests, following subroutine is expected to be used as the UI
+ * thread's entry point.
+ */
 void        *ui_entry_point(void *);
 
 #endif /* UI_H */
